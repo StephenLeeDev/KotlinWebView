92829c252f853853fd647c806e25f68c4cb22145
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-08-13 20:37:29 +0900

    ADD : Loading Url Feature Implementation

--- app/src/main/java/com/example/kotlinwebview/MainActivity.kt
+++ app/src/main/java/com/example/kotlinwebview/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.kotlinwebview
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("https://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
     }
 }
